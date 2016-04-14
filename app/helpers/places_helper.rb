7e40f5425e4e2feffb18303e8c73f06e157ee8e0
Jeremie <jeremie.bonal@gmail.com>
2016-04-14 04:16:06 +0200

    Small modif on Place#show

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -12,6 +12,10 @@ module PlacesHelper
   end
 
   def map_address(place)
+    "#{place.address}"
+  end
+
+  def full_address(place)
     start = place.name.index("-")
     start ||= -1 # Offsetting the +1
     "#{place.name[(start+1)..-1].strip} #{place.address}"
